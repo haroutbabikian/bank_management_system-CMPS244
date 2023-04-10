f0133f4016826e721c1f731dc39f5336f3a580a5
Harout Babikian <hhbabikian@gmail.com>
2023-04-10 18:26:05 +0300

    commit message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_10_092100) do
-  create_table "accounts", force: :cascade do |t|
+  create_table "accounts", charset: "utf8mb3", force: :cascade do |t|
     t.integer "account_number"
     t.string "account_type"
     t.float "balance"
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_092100) do
     t.index ["customer_id"], name: "index_accounts_on_customer_id"
   end
 
-  create_table "customers", force: :cascade do |t|
+  create_table "customers", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone"
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_092100) do
     t.index ["customer_id"], name: "index_customers_on_customer_id"
   end
 
-  create_table "employees", force: :cascade do |t|
+  create_table "employees", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone"
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_092100) do
     t.index ["name"], name: "index_employees_on_name"
   end
 
-  create_table "loans", force: :cascade do |t|
+  create_table "loans", charset: "utf8mb3", force: :cascade do |t|
     t.integer "loan_id"
     t.string "loan_type"
     t.float "amount"
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_092100) do
     t.index ["loan_id"], name: "index_loans_on_loan_id"
   end
 
-  create_table "transactions", force: :cascade do |t|
+  create_table "transactions", charset: "utf8mb3", force: :cascade do |t|
     t.string "transaction_type"
     t.date "transaction_date"
     t.float "amount"
